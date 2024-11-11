e136cb04bf022f1dd5d8ae7a275511d8545f1347
Tamaz Gociridze <117089051+Tazo-gociridze@users.noreply.github.com>
2024-11-11 14:40:29 +0400

    vercell import error resolve

--- src/components/OtpInput/OtpInputWrapper.tsx
+++ src/components/OtpInput/OtpInputWrapper.tsx
@@ -1,12 +1,43 @@
-import { FC } from "react";
-import useOtpInputWrapperLogic from "@components/OtpInput/useOtpInputWrapperLogic";
+import { FC, useRef, useState } from "react";
 
 const OtpInputWrapper: FC<{ inputsQuantity: number }> = ({
   inputsQuantity,
 }) => {
-  const { handleInputChange, otpValues, inputRefs } = useOtpInputWrapperLogic({
-    inputsQuantity,
-  });
+  const inputRefs = useRef<Array<HTMLInputElement | null>>(
+    Array(inputsQuantity).fill(null),
+  );
+  const [otpValues, setOtpValues] = useState(Array(inputsQuantity).fill(""));
+
+  const handleInputChange = (index: number, value: string) => {
+    const sanitizedValue = value.replace(/[^0-9]/g, "");
+
+    if (sanitizedValue.length > 1) {
+      const digits = sanitizedValue.split("");
+      const newOtpValues = [...otpValues];
+      digits.forEach((digit, i) => {
+        if (i + index < inputsQuantity) {
+          newOtpValues[i + index] = digit;
+          inputRefs.current[i + index]?.focus();
+        }
+      });
+      setOtpValues(newOtpValues);
+      return;
+    }
+
+    setOtpValues((prevValues) => {
+      const newValues = [...prevValues];
+      newValues[index] = sanitizedValue;
+      return newValues;
+    });
+
+    if (sanitizedValue && index < inputsQuantity - 1) {
+      inputRefs.current[index + 1]?.focus();
+    } else if (!sanitizedValue && index > 0) {
+      inputRefs.current[index - 1]?.focus();
+    } else if (index === inputsQuantity - 1) {
+      inputRefs.current[index]?.blur();
+    }
+  };
 
   return (
     <div className="otpinput-wrapper">
